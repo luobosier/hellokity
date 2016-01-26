4ef4bdd93479eb22eb866605ca17d9ba2caa807c
apple-31 <Apple-31@wangdeiMac.local>
2016-01-26 17:46:45 +0800

    xinren

---
+++ github1/github1/DaGeViewController.h
@@ -0,0 +1,13 @@
+//
+//  DaGeViewController.h
+//  github1
+//
+//  Created by apple-31 on 16/1/26.
+//  Copyright (c) 2016年 com.mail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DaGeViewController : UIViewController
+
+@end
---
+++ github1/github1/DaGeViewController.m
@@ -0,0 +1,37 @@
+//
+//  DaGeViewController.m
+//  github1
+//
+//  Created by apple-31 on 16/1/26.
+//  Copyright (c) 2016年 com.mail. All rights reserved.
+//
+
+#import "DaGeViewController.h"
+
+@interface DaGeViewController ()
+
+@end
+
+@implementation DaGeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
