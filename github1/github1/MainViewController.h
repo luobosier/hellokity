70e5cea098f072eecfae62f8af1420cb034dac8c
834937624@qq.com <1468624121@qq.com>
2016-01-26 17:37:05 +0800

    github1

---
+++ github1/github1/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  github1
+//
+//  Created by Apple—07 on 16/1/26.
+//  Copyright (c) 2016年 com.mail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ github1/github1/MainViewController.m
@@ -0,0 +1,38 @@
+//
+//  MainViewController.m
+//  github1
+//
+//  Created by Apple—07 on 16/1/26.
+//  Copyright (c) 2016年 com.mail. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor=[UIColor blackColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
